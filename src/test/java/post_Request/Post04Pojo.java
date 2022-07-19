846b8156aff300cb199165f8a89207a58b254989
Nur-sen <nursenalkan84@gmail.com>
2022-07-19 17:09:13 +0300

    19.07.2022

--- src/test/java/post_Request/Post04Pojo.java
+++ src/test/java/post_Request/Post04Pojo.java
@@ -1,6 +1,17 @@
 package post_Request;
 
-public class Post04Pojo {
+import base_urls.HerokuappBesaUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.BookingDatesPojo;
+import pojos.BookingPojo;
+import pojos.BookingResponseBodyPojo;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Post04Pojo extends HerokuappBesaUrl {
     /*
          Given
          1-   https://restful-booker.herokuapp.com/booking
@@ -35,4 +46,35 @@ public class Post04Pojo {
                                      }
                                   }
      */
+    @Test
+    public  void test01(){
+        //1.Step set the url
+        spec.pathParam("first","booking");
+
+        //2.Step Set the Expecdet Data
+        BookingDatesPojo bookingdates=new BookingDatesPojo("2021-09-21","2021-12-21");
+        BookingPojo requastBody= new BookingPojo("Ali", "Can", 999, true,bookingdates,"Breakfast with white tea, Dragon juice" );
+
+        //3.Step send POST Request get the Response
+       Response response=given().spec(spec).contentType(ContentType.JSON).body(requastBody).when().post("/{first}");
+       response.prettyPrint();
+
+       //4.step Do assertion
+        BookingResponseBodyPojo actualBody=response.as(BookingResponseBodyPojo.class);
+        // 1.yol
+        assertEquals(requastBody.toString(),actualBody.getBooking().toString());
+
+        //2.yol
+        assertEquals(requastBody.getFirstname(),actualBody.getBooking().getFirstname());
+        assertEquals(requastBody.getLastname(),actualBody.getBooking().getLastname());
+        assertEquals(requastBody.getTotalprice(),actualBody.getBooking().getTotalprice());
+        assertEquals(requastBody.getDepositpaid(),actualBody.getBooking().getDepositpaid());
+        assertEquals(requastBody.getBookingdates().getCheckin(),actualBody.getBooking().getBookingdates().getCheckin());
+        assertEquals(requastBody.getBookingdates().getCheckout(),actualBody.getBooking().getBookingdates().getCheckout());
+        assertEquals(requastBody.getAdditionalneeds(),actualBody.getBooking().getAdditionalneeds());
+
+
+
+    }
+
 }
